b70ee3a8433658e2b00a76ff1291ed915b920ed8
peenjeee <gastwocls02@gmail.com>
2025-12-24 17:15:43 +0700

    feat: add job to process and store certificates on the blockchain.

--- app/Jobs/ProcessBlockchainCertificate.php
+++ app/Jobs/ProcessBlockchainCertificate.php
@@ -67,11 +67,9 @@ class ProcessBlockchainCertificate implements ShouldQueue
 
                 // Dispatch IPFS job after blockchain confirms (so metadata includes tx_hash)
                 $ipfsService = new \App\Services\IpfsService();
-                Log::info("Blockchain job: Checking IPFS enabled: " . ($ipfsService->isEnabled() ? 'yes' : 'no'));
                 if ($ipfsService->isEnabled()) {
                     // Refresh certificate to get updated blockchain data
                     $this->certificate->refresh();
-                    Log::info("Blockchain job: Dispatching ProcessIpfsCertificate for {$this->certificate->certificate_number}");
                     \App\Jobs\ProcessIpfsCertificate::dispatch($this->certificate);
                 }
             } else {
