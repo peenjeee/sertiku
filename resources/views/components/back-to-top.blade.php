2ed2ba742eeb28eb5be73a07a4c156c7fbd3bdfc
peenjeee <gastwocls02@gmail.com>
2026-01-17 12:03:08 +0700

    feat: Add a new `back-to-top` component with Alpine.js functionality and styling.

--- resources/views/components/back-to-top.blade.php
+++ resources/views/components/back-to-top.blade.php
@@ -1,5 +1,5 @@
 <div x-data="{ showBackToTop: false }" @scroll.window="showBackToTop = (window.pageYOffset > 300)"
-    class="fixed bottom-6 left-6 z-50 pointer-events-none">
+    class="fixed bottom-6 left-8 z-[999] pointer-events-none">
     <button x-show="showBackToTop" @click="window.scrollTo({top: 0, behavior: 'smooth'})"
         x-transition:enter="transition ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-10 scale-90"
